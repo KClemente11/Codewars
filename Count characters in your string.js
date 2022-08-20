e50705649b781bf50f8c396528b5cd333c03451b
Kyle Clemente <kclemente11@me.com>
2022-08-20 12:05:46 -0500

    Decent 6kyu

---
+++ Count characters in your string.js
@@ -0,0 +1,10 @@
+//The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+//What if the string is empty? Then the result should be empty object literal, {}.
+
+
+function count (string) {  
+    var result = [...string].reduce((prev, curr) => { prev[curr] = prev[curr] ? prev[curr] + 1 : 1; return prev }, {});
+    
+    return result
+  }
