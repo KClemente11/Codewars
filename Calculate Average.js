7d47a55381555e23f7031bf9d85f98eac34af6ef
Kyle Clemente <kclemente11@me.com>
2022-09-18 21:14:58 -0500

    Delete Calculate Average.js

--- Calculate Average.js
+++
@@ -1,9 +0,0 @@
-// Write a function which calculates the average of the numbers in a given list.
-
-// Note: Empty arrays should return 0.
-
-function findAverage(array) {
-    let total = array.reduce((acc,c) => acc + c,0)
-    
-    return total / array.length
-  }
