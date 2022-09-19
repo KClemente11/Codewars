67ed94387605e4557a95b7da8b9d7a1d0d8ccdcc
Kyle Clemente <kclemente11@me.com>
2022-09-18 21:18:09 -0500

    added fun 8kyu

--- Square(n) Sum.js
+++ Square(n) Sum.js
@@ -2,8 +2,11 @@
 
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-function squareSum(numbers){
-    return numbers.reduce((acc, cur) => {
-      return acc + cur ** 2
-    },0)
+function findAverage(array) {
+  if(array.length > 0){
+    let total = array.reduce((acc,c) => acc + c,0)
+   return total / array.length
+  }else{
+    return 0
   }
+}
