33dc4f449b19b8875921c4aafa0edac5c754d798
Kyle Clemente <kclemente11@me.com>
2022-10-25 22:17:31 -0500

    Fun hello world

---
+++ Function 1 - Hello World.js
@@ -0,0 +1,26 @@
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+function greet() {
+    const h = "h"
+    const e = "e"
+    const ll = "ll"
+    const o = "o"
+    const space = " "
+    const w = "w"
+    const r = "r"
+    const l = "l"
+    const d = "d"
+    const bang = "!"
+    
+    return (h + e + ll + o + space + w + o + r + l + d + bang).toString()
+  }
+
+  // or
+
+  function greet() {
+    return "hello world!"
+  }
