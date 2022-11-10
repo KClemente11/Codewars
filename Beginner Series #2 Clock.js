d86dc73ec5162033e0d01faaf0c2152514cb28ad
Kyle Clemente <kclemente11@me.com>
2022-11-10 16:51:57 -0600

    Added clock conversion

---
+++ Beginner Series #2 Clock.js
@@ -0,0 +1,23 @@
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+function past(h, m, s){
+    let hourMilliseconds = h * 3_600_000
+    let minuteMilliseconds = m * 60_000
+    let secondMilliseconds = s * 1000
+    
+    return hourMilliseconds + minuteMilliseconds + secondMilliseconds
+  }
